e75ca1aa7ae53680bf09c43d5fe351587abfe20f
amrharb <65994648+amrharb@users.noreply.github.com>
2022-06-13 18:14:48 +0200

    Time: 72 ms (20.11%), Space: 7.2 MB (90.11%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int dp[505][505];
+    int minDistance(string s, string x) {
+        int n=s.size(),m=x.size();
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(!i&&!j)dp[i][j]=0;
+                else if(!i)dp[i][j]=j;
+                else if(!j)dp[i][j]=i;
+                else if(s[i-1]==x[j-1])dp[i][j]=dp[i-1][j-1];
+                else dp[i][j]=1+min({dp[i][j-1],dp[i-1][j],dp[i-1][j-1]});
+            }
+        }
+        return dp[n][m];
+    }
+};
