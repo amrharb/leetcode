7fbe77865abc37339f9555d68f9b1f807ed593a9
amrharb <65994648+amrharb@users.noreply.github.com>
2022-06-14 09:53:04 +0200

    Time: 168 ms (28.56%), Space: 14.5 MB (55.62%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    long long dp[12][10004];
-    vector<int>v;
-    long long solve(int i,int rem){
-        if(i==v.size()||rem<=0)return abs(rem)*1e6;
-        long long &ret=dp[i][rem];
-        if(~ret)return ret;
-        return ret=min(1+solve(i,rem-v[i]),solve(i+1,rem));
-    }
     int coinChange(vector<int>& coins, int amount) {
-        memset(dp,-1,sizeof dp);
-        v=coins;
-        int ans=solve(0,amount);
-        if(ans>=1e6)ans=-1;
+        int n=coins.size();
+        vector<int>dp(amount+1,1e9);
+        dp[0]=0;
+        for(int i=0;i<n;i++){
+            for(int j=coins[i];j<=amount;j++){
+                dp[j]=min(dp[j],1+dp[j-coins[i]]);
+            }
+        }
+        int ans=dp[amount];
+        if(ans>=1e9)ans=-1;
         return ans;
     }
 };
