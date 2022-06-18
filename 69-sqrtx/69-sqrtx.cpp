423d54d5fdd8c282d7ee843d3547f960fb8995fb
amrharb <65994648+amrharb@users.noreply.github.com>
2022-06-19 00:49:04 +0200

    Time: 0 ms (100.00%), Space: 5.9 MB (25.13%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long l=0,r=x,ans;
+        while(l<=r){
+            long long mid=l+(r-l)/2;
+            if(mid*mid<=x)l=mid+1,ans=mid;
+            else r=mid-1;
+        }
+        return ans;
+    }
+};
