e8bd032da4781643234a967bcc86467e9e14b606
amrharb <65994648+amrharb@users.noreply.github.com>
2022-06-19 00:46:52 +0200

    Time: 5 ms (13.72%), Space: 6.5 MB (32.44%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>>dp(n);
+        for(int i=0;i<n;i++){
+            for(int j=0,k;j<=i;j++){
+                if(!j||j==i)k=1;
+                else k=dp[i-1][j-1]+dp[i-1][j];
+                   dp[i].push_back(k);}}
+        return dp;
+    }
+};
