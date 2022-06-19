3e49fe23766e7b3c1ed55f8d9409cf9b348d96fa
amrharb <65994648+amrharb@users.noreply.github.com>
2022-06-20 01:43:21 +0200

    Time: 99 ms (66.09%), Space: 30.4 MB (39.44%) - LeetHub

---
+++ 542-01-matrix/542-01-matrix.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int dx[4]={0,0,1,-1};
+    int dy[4]={1,-1,0,0};
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size(),m=mat[0].size();
+        queue<pair<int,int>>q;
+        vector<vector<int>>cost(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!mat[i][j]){
+                    q.push({i,j});
+                }
+            }
+        }
+        int steps=0;
+        while(!q.empty()){
+            int sz=q.size();
+            while(sz--){
+                int x=q.front().first,y=q.front().second;
+                q.pop();
+                cost[x][y]=steps;
+                for(int i=0;i<4;i++){
+                    int nx=x+dx[i],ny=y+dy[i];
+                    if(nx>=0&&nx<n&&ny>=0&&ny<m&&mat[nx][ny]){
+                        q.push({nx,ny});
+                        mat[nx][ny]=0;
+                    }
+                }
+            }
+            steps++;
+        }
+        return cost;
+    }
+};
