4c2890883da02f7b2a0da52f8a240900e3df5910
amrharb <65994648+amrharb@users.noreply.github.com>
2022-06-20 06:01:26 +0200

    Time: 0 ms (100.00%), Space: 8.4 MB (39.15%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
@@ -3,11 +3,8 @@ public:
     int sumOddLengthSubarrays(vector<int>& arr) {
         int n=arr.size(),tot=0;
         for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=i;j<n;j++){
-                sum+=arr[j];
-                if((j-i+1)&1)tot+=sum;
-            }
+            int numOfTimes=(i+1)*(n-i);
+            tot+=arr[i]*((numOfTimes+1)/2);
         }
         return tot;
     }
