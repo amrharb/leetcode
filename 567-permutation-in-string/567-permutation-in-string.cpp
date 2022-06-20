9679032e06bd8610135300af8fb733a229ec8b13
amrharb <65994648+amrharb@users.noreply.github.com>
2022-06-20 05:41:38 +0200

    Time: 165 ms (13.04%), Space: 7.8 MB (27.69%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -2,12 +2,18 @@ class Solution {
 public:
     bool checkInclusion(string s, string x) {
         int n=s.size(),m=x.size();
-        string k;
-        sort(s.begin(),s.end());
-        for(int i=0;i<=m-n;i++){
-            k=x.substr(i,n);
-            sort(k.begin(),k.end());
-            if(k==s)return 1;
+        map<char,int>mp,mk;
+        for(auto&it:s)mp[it]++;
+        int l=0,r=0;
+        bool flag=1;
+        for(int i=0;i<m;i++){
+            if(i>=n)mk[x[i-n]]--;
+            mk[x[i]]++;
+            flag=1;
+            for(char j='a';j<='z';j++){
+                if(mp[j]!=mk[j])flag=0;
+            }
+            if(flag)return 1;
         }
         return 0;
     }
