1990f6b076e59cb68ce8c36aa80aeea40e833e4d
amrharb <65994648+amrharb@users.noreply.github.com>
2022-06-21 05:38:41 +0200

    Time: 16 ms (79.14%), Space: 11.3 MB (11.66%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {
-        map<int,map<int,int>>mp;
         int i=0,j=0,n=mat.size(),sum=0;
         while(i<n&&j<n){
-            mp[i][j]=1;
             sum+=mat[i++][j++];
         }
         i=0,j=n-1;
         while(i<n&&j>=0){
-            if(mp[i][j]){i++,j--;continue;}
             sum+=mat[i++][j--];
         }
+        if(n&1)sum-=mat[n/2][n/2];
         return sum;
     }
 };
