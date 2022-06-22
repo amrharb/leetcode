e650d588922ec7f0cfc7d0ccc04282d24e66d5d9
amrharb <65994648+amrharb@users.noreply.github.com>
2022-06-22 05:19:03 +0200

    Time: 3 ms (76.63%), Space: 6.5 MB (98.83%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        for(int i=0;i<s.size();i++){
-            if(s[i]!=t[i])return t[i];
-        }
-        return t.back();
+        int sum=0;
+        for(auto&it:t)sum+=it;
+        for(auto&it:s)sum-=it;
+        return sum;
     }
 };
