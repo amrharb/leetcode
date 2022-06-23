7770e3f7029807f9cba3eba04aa24ad2e57d5620
amrharb <65994648+amrharb@users.noreply.github.com>
2022-06-23 16:02:18 +0200

    Time: 13 ms (36.71%), Space: 9.9 MB (82.38%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -1,13 +1,33 @@
 class Solution {
 public:
-    bool checkIfExist(vector<int>& v) {
-        int n=v.size();
-        map<int,int>mp;
+    bool checkIfExist(vector<int>& arr) {
+        int n=arr.size(),cnt=0;
+        sort(arr.begin(),arr.end());
         for(int i=0;i<n;i++){
-            if(mp[v[i]*2])return 1;
-            if(v[i]%2==0&&mp[v[i]/2])return 1;
-            mp[v[i]]++;
+            if(arr[i]>=0){
+            int l=i+1,r=n-1;
+            while(l<=r){
+                int mid=(l+r)/2;
+                if(arr[mid]==arr[i]*2){
+                    cnt++;
+                    break;
+                }
+                else if(arr[mid]>arr[i]*2)r=mid-1;
+                else l=mid+1;
+            }}
+            else{
+                int l=0,r=i-1;
+            while(l<=r){
+                int mid=(l+r)/2;
+                if(arr[mid]==arr[i]*2){
+                    cnt++;
+                    break;
+                }
+                else if(arr[mid]>arr[i]*2)r=mid-1;
+                else l=mid+1;
+            }
+            }
         }
-        return 0;
+        return cnt;
     }
 };
