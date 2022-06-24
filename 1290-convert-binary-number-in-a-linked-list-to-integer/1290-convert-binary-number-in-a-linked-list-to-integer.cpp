8eddc278bc556cec3005f07477b63425d9f956d5
amrharb <65994648+amrharb@users.noreply.github.com>
2022-06-24 06:27:41 +0200

    Time: 0 ms (100.00%), Space: 8.2 MB (92.78%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -10,21 +10,12 @@
  */
 class Solution {
 public:
-    ListNode *k;
-    void solve(ListNode* head,ListNode *last){
-        if(head==nullptr){
-            k=last;
-            return;
-        }
-        solve(head->next,head);
-        head->next=last;
-    }
-    int count(ListNode* cur,int i){
-        if(cur==nullptr)return 0;
-        return (cur->val==1 ? (1<<i):0)+count(cur->next,i+1);
-    }
     int getDecimalValue(ListNode* head) {
-         solve(head,nullptr);
-        return count(k,0);
+        int sum=0;
+        while(head!=nullptr){
+            sum=(sum*2)+head->val;
+            head=head->next;
+        }
+        return sum;
     }
 };
