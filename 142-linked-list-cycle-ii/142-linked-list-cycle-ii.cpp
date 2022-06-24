a893af4522159e56197ba421c622a524b43d3cf0
amrharb <65994648+amrharb@users.noreply.github.com>
2022-06-24 07:13:45 +0200

    Time: 22 ms (13.75%), Space: 9.9 MB (6.47%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -8,14 +8,12 @@
  */
 class Solution {
 public:
-    map<ListNode*,bool>mp;
-    ListNode* dfs(ListNode* head,ListNode* last){
-        if(head==nullptr)return head;
-        if(mp[head])return head;
-        mp[head]=1;
-        return dfs(head->next,head);
-    }
     ListNode *detectCycle(ListNode *head) {
-        return dfs(head,nullptr);
+        map<ListNode*,bool>mp;
+        while(head!=nullptr&&!mp[head]){
+            mp[head]=1;
+            head=head->next;
+        }
+        return head;
     }
 };
