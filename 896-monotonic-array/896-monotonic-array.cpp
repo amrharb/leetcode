721b1a1643db03e46842480f176d7b075b634e2f
amrharb <65994648+amrharb@users.noreply.github.com>
2022-06-27 15:47:06 +0200

    Time: 319 ms (14.83%), Space: 96.5 MB (64.68%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.cpp
+++ 896-monotonic-array/896-monotonic-array.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& v) {
-        bool flag=1;
+        bool flag1=1,flag2=1;
         int n=v.size();
         for(int i=1;i<n;i++){
-            flag&=(v[i]<=v[i-1]);
+            flag1&=(v[i]<=v[i-1]);
+            flag2&=(v[i]>=v[i-1]);
         }
-        if(flag)return true;
-        flag=1;
-        for(int i=1;i<n;i++){
-            flag&=(v[i]>=v[i-1]);
-        }
-        return flag;
+        return flag1||flag2;
     }
 };
