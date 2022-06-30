20ee5e556a02aa89bd8e8befc0d8e3a8f48cb0eb
Amr Harb <65994648+amrharb@users.noreply.github.com>
2022-06-30 04:23:18 +0200

    Time: 400 ms (16.48%), Space: 127.7 MB (5.90%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    int n;
-    vector<int>v;
-    bool check(int k){
-        int cnt=n,last=0;
-        for(int i=0;i<v.size()&&cnt;i++){
-            if(v[i]>=last){
-                cnt--;
-                last=v[i]+k;
-            }
+    bool check(int k,vector<int>v,int cnt){
+        for(int i=0;i<v.size()&&cnt;){
+            cnt--;
+            i=lower_bound(v.begin()+i,v.end(),v[i]+k)-v.begin();
         }
         return !cnt;
     }
     int maxDistance(vector<int>& position, int m) {
         sort(position.begin(),position.end());
-        n=m,v=position;
         int l=0,r=1e9;
         while(l<=r){
             int mid=l+r>>1;
-            if(check(mid))l=mid+1;
+            if(check(mid,position,m))l=mid+1;
             else r=mid-1;
         }
         return l-1;
