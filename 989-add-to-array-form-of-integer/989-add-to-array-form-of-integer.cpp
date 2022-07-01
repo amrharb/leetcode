7ad1795381e9c139f749aac3ac946f4f5c5a5162
Amr Harb <65994648+amrharb@users.noreply.github.com>
2022-07-01 05:05:35 +0200

    Time: 71 ms (9.16%), Space: 27.3 MB (85.46%) - LeetHub

--- 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.cpp
+++ 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     vector<int> addToArrayForm(vector<int>& num, int k) {
+        reverse(num.begin(),num.end());
         int carry=k;
-        for(int i=num.size()-1;i>=0;i--){
-            num[i]+=carry;
-            carry=num[i]/10;
-            num[i]%=10;
+        for(auto&it:num){
+            it+=carry;
+            carry=it/10;
+            it%=10;
         }
-        while(carry)num.insert(num.begin(),carry%10),carry/=10;
+        while(carry)num.push_back(carry%10),carry/=10;
+        reverse(num.begin(),num.end());
         return num;
     }
 };
