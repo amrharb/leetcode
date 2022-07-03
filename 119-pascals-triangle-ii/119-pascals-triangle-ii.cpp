8e8f9bd4ce2f86761de9140b236136d1f09082ef
Amr Harb <65994648+amrharb@users.noreply.github.com>
2022-07-03 20:07:10 +0200

    Time: 0 ms (100.00%), Space: 6.6 MB (16.60%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> getRow(int n) {
+        vector<vector<int>>dp(n+1);
+        n++;
+        for(int i=0;i<n;i++){
+
+            for(int j=0,k;j<=i;j++){
+
+                if(!j||j==i)k=1;
+
+                else k=dp[i-1][j-1]+dp[i-1][j];
+
+                   dp[i].push_back(k);}}
+
+        return dp.back();
+    }
+};
