08acf1b5e929f7c4074d631db21e63882ba0492a
Amr Harb <65994648+amrharb@users.noreply.github.com>
2022-07-09 08:26:47 +0200

    Time: 957 ms (5.02%), Space: 201.5 MB (5.03%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int n;
+    vector<int>a;
+    set<vector<int>>ans;
+    void solve(int i,int rem,vector<int>v){
+        if(i==n||!rem){
+            if(!rem)ans.insert(v);
+            return;
+        }
+        solve(i+1,rem,v);
+        if(rem>=a[i]){
+            v.push_back(a[i]);
+            solve(i,rem-a[i],v);
+            solve(i+1,rem-a[i],v);
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        n=candidates.size();
+        a=candidates;
+        solve(0,target,{});
+        vector<vector<int>>k(ans.begin(),ans.end());
+        return k;
+    }
+};
