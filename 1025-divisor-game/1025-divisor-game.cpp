57b495a0ece2227a2d054984e312f4307a4de015
Amr Harb <65994648+amrharb@users.noreply.github.com>
2022-07-13 23:28:13 +0200

    Time: 0 ms (100.00%), Space: 5.7 MB (99.11%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
-    int dp[1005];
-    bool solve(int i){
-        int &ret=dp[i];
-        if(~ret)return ret;
-        ret=0;
-        for(int j=1;j*j<=i;j++){
-            if(i!=j&&i%j==0){
-                return ret=!solve(i-j);
-            }
-        }
-        return ret;
-    }
     bool divisorGame(int n) {
-        memset(dp,-1,sizeof dp);
-        return solve(n);
+        return !(n&1);
     }
 };
