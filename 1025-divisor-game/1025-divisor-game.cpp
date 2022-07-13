02ea6312b1d76d59c94ad8dd3852ebe6c2e34e2f
Amr Harb <65994648+amrharb@users.noreply.github.com>
2022-07-13 23:27:44 +0200

    Time: 5 ms (21.47%), Space: 5.9 MB (71.26%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -5,8 +5,8 @@ public:
         int &ret=dp[i];
         if(~ret)return ret;
         ret=0;
-        for(int j=1;j<i;j++){
-            if(i%j==0){
+        for(int j=1;j*j<=i;j++){
+            if(i!=j&&i%j==0){
                 return ret=!solve(i-j);
             }
         }
