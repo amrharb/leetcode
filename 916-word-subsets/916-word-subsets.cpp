ec1eccd85e8156e0be878d7d9436b270b019b826
Amr Harb <65994648+amrharb@users.noreply.github.com>
2022-07-30 09:54:53 +0200

    Time: 262 ms (67.19%), Space: 70.5 MB (80.00%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mp[30],mk[100000][30],mx[30];
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        int n=words1.size(),m=words2.size();
+        for(int i=0;i<m;i++){
+            for(auto&it:words2[i])mp[it-'a']++;
+            for(int k=0;k<26;k++)mx[k]=max(mx[k],mp[k]),mp[k]=0;
+        }
+        for(int i=0;i<n;i++){
+            for(auto&it:words1[i])mk[i][it-'a']++;
+        }
+        vector<string>ans;
+        for(int i=0;i<n;i++){
+            bool flag=1;
+            for(int k=0;k<26;k++){
+                if(mx[k]>mk[i][k])flag=0;
+            }
+            if(flag)ans.push_back(words1[i]);
+        }
+        return ans;
+    }
+};
