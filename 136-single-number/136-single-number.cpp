1430739c371633b73a34b8705cad6043b1c7839f
Amr Harb <65994648+amrharb@users.noreply.github.com>
2022-09-21 21:45:03 +0200

    Time: 54 ms (20.05%), Space: 16.9 MB (53.66%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int sol=0;
-        for(auto&it:nums)sol^=it;
-        return sol;
+        int x=0;
+        for(auto&it:nums)x^=it;
+        return x;
     }
 };
