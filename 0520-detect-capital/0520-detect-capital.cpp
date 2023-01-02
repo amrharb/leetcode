a022389b949d99c139acd45b40cb575de60b3305
Amr Harb <65994648+amrharb@users.noreply.github.com>
2023-01-02 19:50:33 +0200

    Time: 0 ms (100.00%), Space: 5.9 MB (89.31%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -4,9 +4,9 @@ public:
         int lower=0,upper=0;
         int n=word.size();
         for(auto&it:word){
-            lower+=(bool)islower(it);
-            upper+=(bool)isupper(it);
+            lower+=(it>='a'&&it<='z');
+            upper+=(it>='A'&&it<='Z');
         }
-        return (lower==n||upper==n||isupper(word[0])&&upper==1);
+        return (lower==n||upper==n||(word[0]>='A'&&word[0]<='Z')&&upper==1);
     }
 };
