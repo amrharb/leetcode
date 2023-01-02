f4ea0730d48d44c751d5787dfad4a1d70052224b
Amr Harb <65994648+amrharb@users.noreply.github.com>
2023-01-02 19:49:15 +0200

    Time: 4 ms (37.09%), Space: 6.1 MB (24.57%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -3,9 +3,9 @@ public:
     bool detectCapitalUse(string word) {
         int lower=0,upper=0;
         int n=word.size();
-        for(int i=0;i<n;i++){
-            lower+=(bool)islower(word[i]);
-            upper+=(bool)isupper(word[i]);
+        for(auto&it:word){
+            lower+=(bool)islower(it);
+            upper+=(bool)isupper(it);
         }
         return (lower==n||upper==n||isupper(word[0])&&upper==1);
     }
