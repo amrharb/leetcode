014aa809c900b86fc12d674714a3aa6152ab33d2
Amr Harb <65994648+amrharb@users.noreply.github.com>
2023-01-03 02:14:12 +0200

    Time: 65 ms (58.18%), Space: 12.2 MB (72.46%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -3,11 +3,11 @@ public:
     int minDeletionSize(vector<string>& strs) {
         int n=strs.size(),m=strs[0].size(),ans=0;
         for(int i=0;i<m;i++){
-            bool flag=1;
+            bool flag=0;
             for(int j=1;j<n;j++){
-                flag&=(strs[j][i]>=strs[j-1][i]);
+                flag|=(strs[j][i]<strs[j-1][i]);
             }
-            ans+=!flag;
+            ans+=flag;
         }
         return ans;
     }
