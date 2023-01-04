8ab023a7543cf9a488bdf781e1e29ae597143d08
Amr Harb <65994648+amrharb@users.noreply.github.com>
2023-01-04 11:57:30 +0200

    Time: 338 ms (55.68%), Space: 94.9 MB (96.59%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int minimumRounds(vector<int>& tasks) {
-        map<int,int>mp;
+        int n=tasks.size();
         bool flag=0;
-        int ans=0;
-        for(auto&it:tasks)mp[it]++;
-        for(auto&it:mp)flag|=(it.second==1),ans+=(it.second+2)/3;
+        int ans=0,cnt=1;
+        sort(tasks.begin(),tasks.end());
+        for(int i=1;i<n;i++){
+            if(tasks[i]==tasks[i-1])cnt++;
+            else flag|=(cnt==1),ans+=(cnt+2)/3,cnt=1;
+        }
+        flag|=(cnt==1);
+        ans+=(cnt+2)/3;
         if(flag)return -1;
         return ans;
-        
     }
 };
