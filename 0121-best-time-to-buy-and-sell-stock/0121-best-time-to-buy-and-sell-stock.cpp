2dda60ee675efe65623ffe356a2809e23dfac9a4
Amr Harb <65994648+amrharb@users.noreply.github.com>
2023-04-04 13:00:24 +0200

    Time: 145 ms (33.71%), Space: 93.4 MB (57.50%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxProfit(vector<int>& v) {
         int mn=v[0],ans=0;
-        for(auto&it:v){
+        for(auto it:v){
             if(it-mn>ans)
                 ans=it-mn;
             if(it<mn)
