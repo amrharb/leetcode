50a38d806adcb09af41b1a0fdc37d5fbc3b163bc
Amr Harb <65994648+amrharb@users.noreply.github.com>
2023-04-08 21:07:10 +0200

    Time: 8 ms (65.20%), Space: 11.6 MB (8.83%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -1,23 +1,16 @@
 class Solution {
+private:
+    unordered_map<Node*,Node*>cloned;
 public:
     Node* copyRandomList(Node* head) {
         if(head==nullptr)
             return nullptr;
-        unordered_map<Node*,Node*>cloned;
-        Node* temp=head;
-        while(temp!=nullptr){
-            Node* newNode=new Node(temp->val);
-            cloned[temp]=newNode;
-            temp=temp->next;
-        }
-        temp=head;
-        while(temp!=nullptr){
-            if(temp->next!=nullptr)
-                cloned[temp]->next=cloned[temp->next];
-            if(temp->random!=nullptr)
-                cloned[temp]->random=cloned[temp->random];
-            temp=temp->next;
-        }
-        return cloned[head];
+        if(cloned.find(head)!=cloned.end())
+            return cloned[head];
+        Node* newNode=new Node(head->val);
+        cloned[head]=newNode;
+        newNode->next=copyRandomList(head->next);
+        newNode->random=copyRandomList(head->random);
+        return newNode;
     }
 };
