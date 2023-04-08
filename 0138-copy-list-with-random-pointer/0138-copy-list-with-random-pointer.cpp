114a502c50f6f1fec30fba0b09d8eb9ac70dda95
Amr Harb <65994648+amrharb@users.noreply.github.com>
2023-04-08 19:47:39 +0200

    Time: 15 ms (13.76%), Space: 11.3 MB (68.06%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -1,32 +1,12 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* next;
-    Node* random;
-    
-    Node(int _val) {
-        val = _val;
-        next = NULL;
-        random = NULL;
-    }
-};
-*/
-
 class Solution {
-private:
-    unordered_map<Node*,Node*>cloned;
 public:
     Node* copyRandomList(Node* head) {
-        if(head==nullptr)
-            return nullptr;
-        if(cloned.find(head)!=cloned.end())
-            return cloned[head];
-        Node* newNode=new Node(head->val);
-        cloned[head]=newNode;
-        newNode->next=copyRandomList(head->next);
-        newNode->random=copyRandomList(head->random);
-        return newNode;
+        if(!head)   return head;
+        unordered_map<Node*, Node*> copy;
+        Node* it = head;
+        while(it) if(!copy[it]) copy[it] = new Node(it->val), it = it->next;
+        it = head;
+        while(it) copy[it]->next = it->next ? copy[it->next] : nullptr, copy[it]->random = it->random ? copy[it->random] : nullptr, it = it->next;
+        return copy[head];
     }
 };
