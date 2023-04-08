bac407d985e4a2b1c69b14ae05854756cff734a6
Amr Harb <65994648+amrharb@users.noreply.github.com>
2023-04-08 21:58:32 +0200

    Time: 17 ms (80.33%), Space: 17.8 MB (99.96%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,12 +3,10 @@ public:
     int missingNumber(vector<int>& nums) {
         bitset<10001>bs;
         int n=nums.size();
-        for(int i=0;i<=n;i++)
-            bs[i]=1;
         for(int i=0;i<n;i++)
-            bs[nums[i]]=0;
+            bs[nums[i]]=1;
         for(int i=0;i<n;i++)
-            if(bs[i])
+            if(!bs[i])
                 return i;
         return n;
     }
