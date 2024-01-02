93da04beb4c2b8b810362d0d64cb9136393c1426
Amr Harb <65994648+amrharb@users.noreply.github.com>
2024-01-02 22:07:31 +0200

    Time: 27 ms (7.89%), Space: 29.4 MB (99.76%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,12 +1,13 @@
+int freq[201];
 class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
-        unordered_map<int,int>freq;
+        memset(freq,0,sizeof freq);
         int mx=0;
         for(auto&it:nums)
             mx=max(mx,++freq[it]);
         vector<vector<int>>ans(mx);
-        freq.clear();
+        memset(freq,0,sizeof freq);
         for(auto&it:nums)
             ans[freq[it]++].push_back(it);
         return ans;
