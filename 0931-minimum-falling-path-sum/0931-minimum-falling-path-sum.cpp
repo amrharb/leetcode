1031682beebe281c7f4c060e352104840b191a07
Amr Harb <65994648+amrharb@users.noreply.github.com>
2024-01-19 14:12:54 +0200

    Time: 7 ms (97.86%), Space: 13.2 MB (5.13%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& v) {
-        int n=v.size(),m=v[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,1e9));
-        for(int j=0;j<m;j++)
-            dp[0][j]=v[0][j];
-        for(int i=1;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                for(int k=-1;k<=1;k++)
-                {
-                    if(j+k>=0&&j+k<m)
-                        dp[i][j]=min(dp[i][j],v[i][j]+dp[i-1][j+k]);
-                }
+        int n=v.size();
+        vector<vector<int>>dp;
+        dp=v;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<n ;j++){
+                dp[i][j]=dp[i-1][j];
+                if(j-1>=0)dp[i][j]=min(dp[i-1][j-1],dp[i][j]);
+                if(j+1<n)dp[i][j]=min(dp[i-1][j+1],dp[i][j]);
+                dp[i][j]+=v[i][j];
             }
         }
-        int mn=1e9;
-        for(int j=0;j<m;j++)
-            mn=min(mn,dp[n-1][j]);
-        return mn;
+        return *min_element(dp[n-1].begin(),dp[n-1].end());
     }
 };
