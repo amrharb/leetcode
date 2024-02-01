5d33eb9d9f0d5af8bb2a03ae2eb6d056a16f8dba
Amr Harb <65994648+amrharb@users.noreply.github.com>
2024-02-01 06:35:20 +0200

    Time: 129 ms (99.04%), Space: 110.2 MB (95.48%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
@@ -8,7 +8,9 @@ public:
             if (nums[i + 2] - nums[i] > k) {
                 return {};
             }
-            ans[i/3]={nums[i], nums[i + 1], nums[i + 2]};
+            ans[i/3][0]=nums[i];
+            ans[i/3][1]=nums[i + 1];
+            ans[i/3][2]=nums[i + 2];
         }
         return ans;
     }
