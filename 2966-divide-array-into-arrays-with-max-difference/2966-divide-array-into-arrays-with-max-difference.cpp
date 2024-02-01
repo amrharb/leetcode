689241dd56c8b95167a2d2baac98d5168a3a18e2
Amr Harb <65994648+amrharb@users.noreply.github.com>
2024-02-01 06:36:16 +0200

    Time: 141 ms (93.38%), Space: 110.1 MB (95.48%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     vector<vector<int>> divideArray(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
-        int n=nums.size();
-        vector<vector<int>> ans(n/3,{0,0,0});
-        for (int i = 0; i < n; i += 3) {
+        vector<vector<int>> ans(nums.size()/3,vector<int>(3));
+        for (int i = 0; i < nums.size(); i += 3) {
             if (nums[i + 2] - nums[i] > k) {
                 return {};
             }
-            ans[i/3][0]=nums[i];
-            ans[i/3][1]=nums[i + 1];
-            ans[i/3][2]=nums[i + 2];
+            ans[i/3]={nums[i], nums[i + 1], nums[i + 2]};
         }
         return ans;
     }
