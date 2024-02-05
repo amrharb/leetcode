64c48ec8c382c20a9f6f67529dfb91860b5ec3d7
Amr Harb <65994648+amrharb@users.noreply.github.com>
2024-02-05 20:41:28 +0200

    Time: 15 ms (97.21%), Space: 11.9 MB (27.34%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    int mp[128];
+    int mp[26];
     int firstUniqChar(string s) {
-        for(auto it:s)mp[it]++;
+        for(auto it:s)mp[it-'a']++;
         for(int i=0;i<s.size();i++){
-            if(mp[s[i]]==1)return i;
+            if(mp[s[i]-'a']==1)return i;
         }
         return -1;
     }
