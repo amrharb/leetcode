1379ddf1f60854addfb900016920c10ea77dab4a
Amr Harb <65994648+amrharb@users.noreply.github.com>
2024-02-05 21:13:33 +0200

    Time: 153 ms (5.04%), Space: 10.7 MB (6.28%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    bool check(int s[128],int t[128])
+    unordered_map<char,int> ft,cur;
+    bool check()
     {
-        for(int i=0;i<128;i++)
-            if(s[i]<t[i])
+        for(auto&[a,b]:ft)
+            if(b>cur[a])
                 return 0;
         return 1;
     }
     string minWindow(string s, string t) {
         int n=s.size(),m=t.size();
-        int ft[128]={},cur[128]={};
         for(int i=0;i<m;i++)
             ft[t[i]]++;
         int i=0,j=0,mn=1e9,l=0,r=-1;
         while(i<n)
         {
-            while(j<n&&!check(cur,ft))
+            while(j<n&&!check())
             {
                 cur[s[j++]]++;
             }
-            if(check(cur,ft)&&j-i<mn)
+            if(check()&&j-i<mn)
                 mn=j-i,l=i,r=j-1;
             cur[s[i++]]--;
         }
