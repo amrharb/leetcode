944a3bd7f54b20cd4dd3773ea269e341198de318
Amr Harb <65994648+amrharb@users.noreply.github.com>
2024-02-10 11:59:02 +0200

    Time: 749 ms (5.00%), Space: 417.9 MB (8.47%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,15 +1,27 @@
+const int N=1005;
+int dp[N][N];
 class Solution {
 public:
+    
+    bool solve(string s,int i,int j)
+    {
+        if(i>=j)
+            return 1;
+        int &ret=dp[i][j];
+        if(~ret)
+            return ret;
+        if(s[i]==s[j])
+            return ret=solve(s,i+1,j-1);
+        return ret=0;
+    }
     int countSubstrings(string s) {
+        memset(dp,-1,sizeof dp);
         int n=s.size(),cnt=0;
         for(int i=0;i<n;i++)
         {
-            deque<char>forward,backward;
             for(int j=i;j<n;j++)
             {
-                forward.push_back(s[j]);
-                backward.push_front(s[j]);
-                cnt+=(forward==backward);
+                cnt+=solve(s,i,j);
             }
         }
         return cnt;
