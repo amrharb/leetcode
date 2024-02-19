f678dc9e71ee70cff2bce445dd159f055bce54d3
Amr Harb <65994648+amrharb@users.noreply.github.com>
2024-02-19 23:34:39 +0200

    Time: 0 ms (100.00%), Space: 7.3 MB (19.56%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
-    bool isPowerOfTwo(int n) {
-        if(n<=0)return 0;
-        int cnt=0;
-        while(n){
-            cnt+=n%2;
-            n/=2;
-        }
-        return cnt==1;
+    bool isPowerOfTwo(int x) {
+        if(x<=0)return 0;
+        return (x&(x-1))==0;
     }
 };
