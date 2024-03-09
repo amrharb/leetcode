8bad89457aee43f720eb283ff7311e9a4130d034
Amr Harb <65994648+amrharb@users.noreply.github.com>
2024-03-10 01:36:02 +0200

    Time: 64 ms (82.52%), Space: 53 MB (51.69%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int getCommon(vector<int>& a, vector<int>& b) {
-        int n=a.size(),m=b.size(),i=0,j=0;
-        while(i<n&&j<m)
+        reverse(a.begin(),a.end());
+        reverse(b.begin(),b.end());
+        int i=a.size()-1,j=b.size()-1;
+        while(i>=0&&j>=0)
         {
             if(a[i]==b[j])
                 return a[i];
             if(a[i]>b[j])
-                j++;
+                j--;
             else
-                i++;
+                i--;
         }
         return -1;
     }
