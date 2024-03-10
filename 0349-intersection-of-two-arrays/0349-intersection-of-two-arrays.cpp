668f6e9df83d4e599b93707b742b768a602bcf58
Amr Harb <65994648+amrharb@users.noreply.github.com>
2024-03-10 13:17:28 +0200

    Time: 3 ms (83.79%), Space: 12.4 MB (61.63%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -3,18 +3,14 @@ public:
     vector<int> intersection(vector<int>& a, vector<int>& b) {
         int n=a.size(),m=b.size();
         sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
         vector<int>c;
         for(int i=0;i<n;i++)
         {
             if(i&&a[i]==a[i-1])continue;
-            for(int j=0;j<m;j++)
-            {
-                if(a[i]==b[j])
-                {
-                    c.push_back(a[i]);
-                    break;
-                }
-            }
+            int idx=lower_bound(b.begin(),b.end(),a[i])-b.begin();
+            if(idx!=m&&a[i]==b[idx])
+                c.push_back(a[i]);
         }
         return c;
     }
