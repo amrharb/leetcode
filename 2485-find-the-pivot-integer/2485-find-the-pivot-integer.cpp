5a9b89be66e00fbb4db8864f04a1307869c1c1f2
Amr Harb <65994648+amrharb@users.noreply.github.com>
2024-03-14 00:55:54 +0200

    Time: 0 ms (100.00%), Space: 7.3 MB (42.04%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,9 +1,5 @@
 class Solution {
 public:
-    int sum(int n)
-    {
-        return n*(n+1)/2;
-    }
     int pivotInteger(int n) {
         int x=sqrt(n*(n+1)/2);
         return (x*x==(n*(n+1)/2) ? x:-1);
