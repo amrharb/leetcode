8ed383da0fee5396d115702cb47695472625dfe5
Amr Harb <65994648+amrharb@users.noreply.github.com>
2024-03-14 00:54:26 +0200

    Time: 0 ms (100.00%), Space: 7 MB (88.01%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -5,17 +5,7 @@ public:
         return n*(n+1)/2;
     }
     int pivotInteger(int n) {
-        int l=1,r=n;
-        while(l<=r)
-        {
-            int mid=l+r>>1;
-            if(sum(mid)==sum(n)-sum(mid-1))
-                return mid;
-            if(sum(mid)>sum(n)-sum(mid))
-                r=mid-1;
-            else
-                l=mid+1;
-        }
-        return -1;
+        int x=sqrt(n*(n+1)/2);
+        return (sum(x)==sum(n)-sum(x-1) ? x:-1);
     }
 };
