973e6be51f43d7f69f89b3dd18a2bba395198c7f
Amr Harb <65994648+amrharb@users.noreply.github.com>
2024-03-15 02:08:37 +0200

    Time: 6 ms (98.82%), Space: 26.4 MB (55.36%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -4,26 +4,28 @@ public:
     vector<int> productExceptSelf(vector<int>& nums)
     {
         int n=nums.size(),prod=1,zeros=0;
-        vector<int>pref(n,1),suf(n,1),ans(n,1);
-        for(int i=0;i<n;i++)
+        for(int i=0; i<n; i++)
         {
-            if(i)
-                pref[i]=pref[i-1];
-            pref[i]*=nums[i];
+            if(nums[i])
+                prod*=nums[i];
+            else zeros++;
         }
-        for(int i=n-1;i>=0;i--)
+        if(zeros>1){
+            for(int i=0;i<n;i++)
+                nums[i]=0;
+            return nums;
+        }
+        for(int i=0; i<n; i++)
         {
-            if(i!=n-1)
-                suf[i]=suf[i+1];
-            suf[i]*=nums[i];
+            if(nums[i]){
+                if(zeros)
+                    nums[i]=0;
+                else nums[i]=prod/nums[i];
+            }
+            else{
+                nums[i]=prod;
+            }
         }
-        for(int i=0;i<n;i++)
-        {
-            if(i)
-                ans[i]=pref[i-1];
-            if(i+1<n)
-                ans[i]*=suf[i+1];
-        }
-        return ans;
+        return nums;
     }
 };
