f02c3e339598967fa5157629896aec52ac050da3
Amr Harb <65994648+amrharb@users.noreply.github.com>
2024-04-26 10:27:34 +0300

    Time: 121 ms (68.81%), Space: 50.9 MB (5.07%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -3,18 +3,20 @@ public:
     int minFallingPathSum(vector<vector<int>>& v) {
         int n=v.size(),m=v[0].size();
         vector<vector<int>>dp(n,vector<int>(m,1e9));
+        multiset<int>st;
         for(int j=0;j<m;j++)
-            dp[0][j]=v[0][j];
+            dp[0][j]=v[0][j],st.insert(dp[0][j]);
         for(int i=1;i<n;i++)
         {
+            multiset<int>nw;
             for(int j=0;j<m;j++)
             {
-                for(int k=0;k<m;k++)
-                {
-                    if(j!=k)
-                        dp[i][j]=min(dp[i][j],v[i][j]+dp[i-1][k]);
-                }
+                st.erase(st.find(dp[i-1][j]));
+                dp[i][j]=min(dp[i][j],v[i][j]+*st.begin());
+                st.insert(dp[i-1][j]);
+                nw.insert(dp[i][j]);
             }
+            st=nw;
         }
         int mn=1e9;
         for(int j=0;j<m;j++)
