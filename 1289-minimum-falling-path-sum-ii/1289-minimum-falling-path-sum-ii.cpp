a192a807c97e9f0760b9071437ff1bcdfd42f96c
Amr Harb <65994648+amrharb@users.noreply.github.com>
2024-04-26 10:37:54 +0300

    Time: 134 ms (64.91%), Space: 49.8 MB (5.07%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& v) {
         int n=v.size(),m=v[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,1e9));
+        vector<int>dp(m);
         multiset<int>st;
         for(int j=0;j<m;j++)
-            dp[0][j]=v[0][j],st.insert(dp[0][j]);
+            dp[j]=v[0][j],st.insert(dp[j]);
         for(int i=1;i<n;i++)
         {
             multiset<int>nw;
             for(int j=0;j<m;j++)
             {
-                st.erase(st.find(dp[i-1][j]));
-                dp[i][j]=min(dp[i][j],v[i][j]+*st.begin());
-                st.insert(dp[i-1][j]);
-                nw.insert(dp[i][j]);
+                int last=dp[j];
+                st.erase(st.find(dp[j]));
+                dp[j]=v[i][j]+*st.begin();
+                st.insert(last);
+                nw.insert(dp[j]);
             }
             st=nw;
         }
-        return *min_element(dp[n-1].begin(),dp[n-1].end());
+        return *min_element(dp.begin(),dp.end());
     }
 };
