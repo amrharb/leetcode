1daa8419be2844e8621d97aeafe3d335aea4b1fd
Amr Harb <65994648+amrharb@users.noreply.github.com>
2024-05-02 23:26:11 +0300

    Time: 7 ms (99.26%), Space: 22.7 MB (79.58%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     int findMaxK(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        int idx,n=nums.size();
         for(auto&it:nums)
         {
             if(it>0)break;
-            idx=lower_bound(nums.begin(),nums.end(),-it)-nums.begin();
-            if(idx<nums.size()&&nums[idx]==-it)
+            auto idx=lower_bound(nums.begin(),nums.end(),-it);
+            if(idx!=nums.end()&&*idx==-it)
                 return -it;
         }
         return -1;
