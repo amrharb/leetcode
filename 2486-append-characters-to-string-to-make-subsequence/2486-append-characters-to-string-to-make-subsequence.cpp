befca1cdb1d94d40923577fca22d3450a12beff1
Amr Harb <65994648+amrharb@users.noreply.github.com>
2024-06-03 08:03:02 +0300

    Time: 17 ms (70.29%), Space: 11.9 MB (83.42%) - LeetHub

--- 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     int appendCharacters(string s, string t) {
         int j=0,m=t.size();
-        for(auto&it:s)
-            if(j<m&&it==t[j])
+        for(auto&it:s){
+            if(j==m)break;
+             if(it==t[j])
                 j++;
+        }
         return m-j;
     }
 };
