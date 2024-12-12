514b45723f05371e6bec39ece2f28c1e80c45c4e
Amr Harb <65994648+amrharb@users.noreply.github.com>
2024-12-12 22:35:58 +0200

    Time: 3 ms (54.35%), Space: 13.1 MB (77.22%) - LeetHub

---
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        priority_queue<int>pq;
+        for(auto&it:gifts){
+            pq.push(it);
+        }
+        while(k--){
+            int mx=pq.top();
+            pq.pop();
+            pq.push((int)sqrt(mx));
+        }
+        long long ans=0;
+        while(!pq.empty()){
+            ans+=pq.top();
+            pq.pop();
+        }
+        return ans;
+    }
+};
