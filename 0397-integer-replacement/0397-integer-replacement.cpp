512bd4428d32ba056669d6f298644f1a67a870ce
Amr Harb <65994648+amrharb@users.noreply.github.com>
2024-12-12 15:32:52 +0200

    Time: 0 ms (100.00%), Space: 7.9 MB (39.30%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.cpp
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -4,14 +4,13 @@ public:
     int integerReplacement(ll n) {
         int cnt=0;
         while(n!=1){
-            if(n%2==0)n/=2;
+            if(!(n&1))n/=2;
             else{
-                if((n/2)%2==0||n==3)n--;
-                else if((n/2+1)%2==0)n++;
+                if(!(n&1)||n==3)n--;
+                else if(!((n/2+1)&1))n++;
                 else n--;
             }
             cnt++;
-            cout<<n<<' ';
         }
         return cnt;
     }
