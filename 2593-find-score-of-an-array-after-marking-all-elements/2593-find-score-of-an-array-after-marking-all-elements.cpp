1060acd7507093ef76bb618528c17d3198903f7c
Amr Harb <65994648+amrharb@users.noreply.github.com>
2024-12-13 12:58:14 +0200

    Time: 365 ms (14.44%), Space: 176.6 MB (6.21%) - LeetHub

--- 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
@@ -3,18 +3,19 @@ public:
     long long findScore(vector<int>& nums) {
         int n=nums.size();
         vector<bool>vis(n);
-        vector<pair<int,int>>v;
+        map<int,vector<int>>adj;
         for(int i=0;i<n;i++){
-            v.push_back({nums[i],i});
+            adj[nums[i]].push_back(i);
         }
-        sort(v.begin(),v.end());
         long long ans=0;
-        for(auto [val,idx]:v){
-            if(vis[idx])continue;
-            ans+=val;
-            vis[idx]=1;
-            if(idx)vis[idx-1]=1;
-            if(idx+1<n)vis[idx+1]=1;
+        for(auto [a,indexes]:adj){
+            for(auto idx:indexes){
+                if(vis[idx])continue;
+                ans+=nums[idx];
+                vis[idx]=1;
+                if(idx)vis[idx-1]=1;
+                if(idx+1<n)vis[idx+1]=1;
+            }
         }
         return ans;
     }
