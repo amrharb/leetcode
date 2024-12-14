7b2f7d769b94d3de4fba9c2370ddf3081cab3f80
Amr Harb <65994648+amrharb@users.noreply.github.com>
2024-12-14 20:41:54 +0200

    Time: 0 ms (100.00%), Space: 12.3 MB (21.18%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(),citations.end());
+        int mx=0,n=citations.size();
+        for(int i=0;i<n;i++){
+            mx=max(mx,min(n-i,citations[i]));
+        }
+        return mx;
+    }
+};
