0421fa0a3f11538ddd71450c3f9465cd0f25e8d9
Amr Harb <65994648+amrharb@users.noreply.github.com>
2024-12-17 15:30:43 +0200

    Time: 0 ms (100.00%), Space: 14.1 MB (7.76%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
@@ -14,21 +14,25 @@ public:
     bool isCousins(TreeNode* root, int x, int y) {
         queue<TreeNode*>q;
         q.push(root);
-        vector<int>par(101);
         while(!q.empty()){
             int sz=q.size();
-            int haveX=0,haveY=0;
+            bool haveX=false,haveY=false;
             while(sz--){
                 TreeNode* cur=q.front();
                 q.pop();
-                if(cur->val==x)haveX=par[cur->val];
-                if(cur->val==y)haveY=par[cur->val];
+                if(cur->left!=nullptr&&cur->right!=nullptr){
+                    int left=cur->left->val,right=cur->right->val;
+                    if(min(left,right)==min(x,y)&&max(left,right)==max(x,y))
+                        return false;
+                }
+                if(cur->val==x)haveX=1;
+                if(cur->val==y)haveY=1;
                 if(cur->left!=nullptr)
-                    q.push(cur->left),par[cur->left->val]=cur->val;
+                    q.push(cur->left);
                 if(cur->right!=nullptr)
-                    q.push(cur->right),par[cur->right->val]=cur->val;
+                    q.push(cur->right);
             }
-            if(haveX&&haveY&&haveX!=haveY)
+            if(haveX&&haveY)
                 return true;
         }
         return false;
