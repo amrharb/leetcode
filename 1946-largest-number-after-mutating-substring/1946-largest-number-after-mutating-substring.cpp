23f1a9b6b8de77571d6a338a44b4dd87a0771944
Amr Harb <65994648+amrharb@users.noreply.github.com>
2024-12-25 08:26:06 +0200

    Time: 4 ms (64.67%), Space: 32.4 MB (52.00%) - LeetHub

--- 1946-largest-number-after-mutating-substring/1946-largest-number-after-mutating-substring.cpp
+++ 1946-largest-number-after-mutating-substring/1946-largest-number-after-mutating-substring.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     string maximumNumber(string num, vector<int>& change) {
-        string ans=num;
         bool start=0;
-        int n=num.size();
-        for(int i=0;i<n;i++){
-            if(num[i]-'0'>change[num[i]-'0']){
+        for(char &d:num){
+            if(d-'0'>change[d-'0']){
                 if(start)
                     break;
-            }else if(num[i]-'0'<change[num[i]-'0']){
-                ans[i]=char(change[num[i]-'0']+'0');
+            }else if(d-'0'<change[d-'0']){
+                d=char(change[d-'0']+'0');
                 start=1;
             }
         }
-        return ans;
+        return num;
     }
 };
