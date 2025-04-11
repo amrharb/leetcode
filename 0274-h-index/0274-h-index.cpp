c780e88e14a339c27e0663aa68ac6cec8cace202
Amr Harb <65994648+amrharb@users.noreply.github.com>
2025-04-11 16:48:37 +0200

    Time: 0 ms (100%), Space: 12.2 MB (66.08%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        sort(citations.begin(),citations.end());
-        int mx=0,n=citations.size();
-        for(int i=0;i<n;i++){
-            mx=max(mx,min(n-i,citations[i]));
+        sort(citations.begin(), citations.end());
+        int mx = 0, n = citations.size();
+        for (int i = 0; i < n; i++) {
+            mx = max(mx, min(n - i, citations[i]));
         }
         return mx;
     }
