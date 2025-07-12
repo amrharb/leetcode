ca07a01528937ef4cb4e539cc9b1f384bb535b1b
Amr Harb <65994648+amrharb@users.noreply.github.com>
2025-07-12 20:11:39 +0100

    Time: 1070 ms (5.15%), Space: 68.8 MB (93.2%) - LeetHub

---
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.cpp
@@ -0,0 +1,34 @@
+// Please, upvote if you like it. Thanks :-)
+class Solution {
+public:
+    void dfs(vector<vector<pair<int, double>>>& graph, vector<bool>& vis,
+             int src, int dst, double curres, double& res, double& mn) {
+        if (curres <= mn)
+            return;
+        if (src == dst) {
+            res = max(res, curres);
+            return;
+        }
+        vis[src] = true;
+        for (const auto& cur : graph[src]) {
+            int newSrc = cur.first;
+            double newSrcProb = cur.second;
+            if (!vis[newSrc] && curres * newSrcProb >= res)
+                dfs(graph, vis, newSrc, dst, curres * newSrcProb, res, mn);
+        }
+        vis[src] = false;
+    }
+
+    double maxProbability(int n, vector<vector<int>>& e,
+                          vector<double>& succProb, int src, int dst) {
+        vector<vector<pair<int, double>>> graph(n);
+        for (int i = 0; i < e.size(); i++) {
+            graph[e[i][0]].push_back({e[i][1], succProb[i]});
+            graph[e[i][1]].push_back({e[i][0], succProb[i]});
+        }
+        vector<bool> vis(n, false);
+        double res = 0, mn = 1e-5;
+        dfs(graph, vis, src, dst, 1.0, res, mn);
+        return res;
+    }
+};
