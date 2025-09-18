1dbfbf1611a071889c6d6629e2a8070d29d33f28
Amr Harb <65994648+amrharb@users.noreply.github.com>
2025-09-18 14:38:00 +0300

    Time: 812 ms (5%), Space: 412.6 MB (8.75%) - LeetHub

--- 3678-design-task-manager/3678-design-task-manager.cpp
+++ 3678-design-task-manager/3678-design-task-manager.cpp
@@ -1,32 +1,33 @@
 class TaskManager {
 public:
-    multiset<vector<int>>st;
-    map<int,int>mp;
+    multiset<vector<int>> st;
+    map<int, int> mp;
     TaskManager(vector<vector<int>>& tasks) {
-        for(auto task:tasks)
-            add(task[0],task[1],task[2]);
+        for (auto task : tasks)
+            add(task[0], task[1], task[2]);
     }
-    
+
     void add(int userId, int taskId, int priority) {
-        mp[taskId]=priority;
-        st.insert({priority,taskId,userId});
+        mp[taskId] = priority;
+        st.insert({priority, taskId, userId});
     }
-    
+
     void edit(int taskId, int newPriority) {
-        auto idx=*st.lower_bound({mp[taskId],taskId,0});
+        auto idx = *st.lower_bound({mp[taskId], taskId, 0});
         st.erase(idx);
-        mp[taskId]=newPriority;
-        st.insert({mp[taskId],taskId,idx[2]});
+        mp[taskId] = newPriority;
+        st.insert({mp[taskId], taskId, idx[2]});
     }
-    
+
     void rmv(int taskId) {
-        st.erase(st.lower_bound({mp[taskId],taskId,0}));
+        st.erase(st.lower_bound({mp[taskId], taskId, 0}));
         mp.erase(taskId);
     }
-    
+
     int execTop() {
-        if(st.empty())return -1;
-        int idx=(*st.rbegin())[2];
+        if (st.empty())
+            return -1;
+        int idx = (*st.rbegin())[2];
         st.erase(*st.rbegin());
         return idx;
     }
