8f67f9d74502af7b22199169de4efd3661f1994b
Amr Harb <65994648+amrharb@users.noreply.github.com>
2025-10-24 22:50:00 +0300

    Time: 155 ms (75%), Space: 18.7 MB (5.85%) - LeetHub

---
+++ 2174-next-greater-numerically-balanced-number/2174-next-greater-numerically-balanced-number.py
@@ -0,0 +1,24 @@
+from itertools import permutations
+from bisect import bisect_right
+
+
+class Solution:
+    def nextBeautifulNumber(self, n: int) -> int:
+        m = len(str(n))
+        ans = []
+        def solve(d, cur):
+            if d == 10:
+                if len(cur) >= m:
+                    for p in set(permutations(cur)):
+                        if p[0] == "0":
+                            continue
+                        ans.append(int("".join(p)))
+                return
+            solve(d + 1, cur)
+            if len(cur) + d <= m + 1:
+                solve(d + 1, cur + str(d) * d)
+
+        solve(1, "")
+        ans.sort()
+        idx = bisect_right(ans, n)
+        return ans[idx] if idx < len(ans) else -1
