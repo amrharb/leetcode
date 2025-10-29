6ece232b1bdf3015bf290f6c4811438b60314bfe
Amr Harb <65994648+amrharb@users.noreply.github.com>
2025-10-29 12:38:34 +0300

    Time: 0 ms (100%), Space: 17.6 MB (95.42%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.py
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.py
@@ -1,3 +1,5 @@
 class Solution:
     def smallestNumber(self, n: int) -> int:
-        return (1 << ceil(log2(n + 0.000000000001))) - 1
+        while n & (n + 1):
+            n |= n + 1
+        return n
