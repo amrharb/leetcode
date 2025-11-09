32377d656c0d6a25bd5ee3c967f1b4a6ceb8afd3
Amr Harb <65994648+amrharb@users.noreply.github.com>
2025-11-09 20:29:04 +0200

    Time: 0 ms (100%), Space: 8 MB (6.11%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
@@ -4,7 +4,7 @@ public:
         if (num1 == 0 || num2 == 0)
             return 0;
         if (num1 >= num2)
-            return 1 + countOperations(num1 - num2, num2);
-        return 1 + countOperations(num1, num2 - num1);
+            return num1 / num2 + countOperations(num1 % num2, num2);
+        return num2 / num1 + countOperations(num1, num2 % num1);
     }
 };
