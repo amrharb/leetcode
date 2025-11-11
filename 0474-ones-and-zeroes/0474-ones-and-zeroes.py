c9c544b45efc150c92e9620b9446ceddb53b1ff8
Amr Harb <65994648+amrharb@users.noreply.github.com>
2025-11-11 16:49:19 +0200

    Time: 1622 ms (49.14%), Space: 457.3 MB (5.23%) - LeetHub

---
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        @lru_cache(10**7)
+        def solve(i, zeros, ones):
+            if zeros < 0 or ones < 0:
+                return -(10**5)
+            if i == len(strs):
+                return 0
+            return max(
+                solve(i + 1, zeros, ones),
+                1 + solve(i + 1, zeros - strs[i].count("0"), ones - strs[i].count("1")),
+            )
+
+        return solve(0, m, n)
