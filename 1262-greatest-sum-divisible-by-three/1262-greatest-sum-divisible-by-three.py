ef5616c4cb6a24329881e0bc8f8558f4b100b02f
Amr Harb <65994648+amrharb@users.noreply.github.com>
2025-11-23 02:53:38 +0200

    Time: 171 ms (16.86%), Space: 28.4 MB (13.02%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
@@ -1,11 +1,11 @@
 class Solution:
     def maxSumDivThree(self, nums: List[int]) -> int:
-        @lru_cache(12 * 10**4)
-        def solve(i, rem):
-            if i == len(nums):
-                if rem == 0:
-                    return 0
-                return -(10**9)
-            return max(solve(i + 1, rem), nums[i] + solve(i + 1, (rem + nums[i]) % 3))
-
-        return solve(0, 0)
+        n = len(nums)
+        dp = [[-(10**9) for _ in range(3)] for _ in range(n + 1)]
+        dp[n][0] = 0
+        for i in range(n - 1, -1, -1):
+            for rem in range(3):
+                dp[i][rem] = max(
+                    dp[i + 1][rem], nums[i] + dp[i + 1][(rem + nums[i]) % 3]
+                )
+        return dp[0][0]
