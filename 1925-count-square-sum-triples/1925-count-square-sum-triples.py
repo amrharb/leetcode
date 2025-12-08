52e1f2706e22f284b374891a44a95a0ec4953d75
Amr Harb <65994648+amrharb@users.noreply.github.com>
2025-12-08 06:43:45 +0200

    Time: 86 ms (86.69%), Space: 17.5 MB (94.57%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.py
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.py
@@ -5,9 +5,8 @@ class Solution:
         for a in range(1, n + 1):
             sq[a * a] = 1
         for a in range(1, n + 1):
-            for b in range(1, n + 1):
+            mxB = int(sqrt(n * n - a * a))
+            for b in range(1, mxB + 1):
                 if a * a + b * b in sq:
                     ans += 1
-                elif a * a + b * b > n * n:
-                    break
         return ans
