08316333ad98a9e8223f5a52357ac3af934b0b58
Amr Harb <65994648+amrharb@users.noreply.github.com>
2025-12-09 06:39:18 +0200

    Time: 532 ms (86.49%), Space: 41.4 MB (77.93%) - LeetHub

---
+++ 3583-count-special-triplets/3583-count-special-triplets.py
@@ -0,0 +1,14 @@
+class Solution:
+    def specialTriplets(self, nums: List[int]) -> int:
+        pref = defaultdict(int)
+        suf = defaultdict(int)
+        mod = 10 ** 9 + 7
+        ans = 0
+        for num in nums:
+            suf[num] += 1
+        for num in nums:
+            suf[num] -= 1
+            if num * 2 in pref and num * 2 in suf:
+                ans += pref[num * 2] * suf[num * 2]
+            pref[num] += 1
+        return ans % mod
