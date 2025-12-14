950b60a7b1bd22f626b69a1ba09290373593e410
Amr Harb <65994648+amrharb@users.noreply.github.com>
2025-12-15 01:14:41 +0200

    Time: 3407 ms (5.77%), Space: 111.3 MB (11.54%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
@@ -3,19 +3,15 @@ class Solution:
         n = len(s)
         mod = 10**9 + 7
 
-        dp = [[-1 for _ in range(3)] for _ in range(n)]
-
+        @lru_cache
         def solve(i, cnt):
             if cnt > 2:
                 return 0
             if i == n:
                 return cnt == 2
-            if dp[i][cnt] != -1:
-                return dp[i][cnt]
-            dp[i][cnt] = (
+            return (
                 solve(i + 1, cnt + (s[i] == "S"))
                 + (solve(i + 1, 0) if cnt + (s[i] == "S") == 2 else 0)
             ) % mod
-            return dp[i][cnt]
 
         return solve(0, 0)
