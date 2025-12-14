8af2b3ffdcce0bf5ee1587481603efaedec11c51
Amr Harb <65994648+amrharb@users.noreply.github.com>
2025-12-15 01:28:48 +0200

    Time: 39 ms (58.27%), Space: 17.8 MB (56.9%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,12 +1,10 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        sign = -1 if x < 0 else 1
-        s = str(abs(x))
-        while len(s) > 1 and s[-1] == "0":
-            s = s[:-1]
-        s = s[::-1]
-
-        def normalize(x):
-            return x if x in range(-(2**31), 2**31) else 0
-
-        return normalize(sign * int(s))
+        sign = [1, -1][x < 0]
+        rev, x = 0, abs(x)
+        while x:
+            x, mod = divmod(x, 10)
+            rev = rev * 10 + mod
+        if rev > 2**31 - 1:
+            return 0
+        return sign * rev
