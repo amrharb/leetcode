cde1944b66f6754352ca4c24db8caaa34b78791c
Amr Harb <65994648+amrharb@users.noreply.github.com>
2025-12-15 19:22:14 +0200

    Time: 0 ms (100%), Space: 18 MB (23.17%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.py
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -1,4 +1,4 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
         sign = [1, -1][dividend < 0] * [1, -1][divisor < 0]
-        return min(2**31 - 1, sign * int(abs(dividend) // abs(divisor)))
+        return min(2**31 - 1, sign * (abs(dividend) // abs(divisor)))
