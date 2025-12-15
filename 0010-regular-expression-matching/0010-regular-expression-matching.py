dd5c2bf785a7fd698862e65aa20d0b6518a79372
Amr Harb <65994648+amrharb@users.noreply.github.com>
2025-12-15 14:06:52 +0200

    Time: 7 ms (73.41%), Space: 18.3 MB (9.02%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -10,17 +10,9 @@ class Solution:
                 if i + 1 < n and p[i + 1] == "*":
                     return match(i + 2, j)
                 return False
-            if (p[i] == s[j] or p[i] == ".") and (i + 1 == n or p[i + 1] != "*"):
-                return match(i + 1, j + 1)
+            first_match = p[i] == s[j] or p[i] == "."
             if i + 1 < n and p[i + 1] == "*":
-                if match(i + 2, j):
-                    return True
-                for k in range(j, m):
-                    if s[k] == p[i] or p[i] == ".":
-                        if match(i + 2, k + 1):
-                            return True
-                    else:
-                        break
-            return False
+                return match(i + 2, j) or first_match and match(i, j + 1)
+            return first_match and match(i + 1, j + 1)
 
         return match(0, 0)
