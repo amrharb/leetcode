409a76bdabb2fabfaaaf4a539b51728a71cbaeb7
Amr Harb <65994648+amrharb@users.noreply.github.com>
2025-12-15 16:52:53 +0200

    Time: 3 ms (92.46%), Space: 17.9 MB (55.1%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,29 +1,22 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        ans = set()
         candidates.sort()
-        for i in range(len(candidates) - 1, target - 1, -1):
-            if candidates[i] == candidates[i - target]:
-                candidates.pop(i)
-
-        n = len(candidates)
-
-        def backTrack(i, curr_sum, curr_lst):
-            if curr_sum > target:
-                return
+        res = []
+        def backtrack(start, curr_sum, path):
             if curr_sum == target:
-                ans.add(tuple(curr_lst))
+                res.append(path[:])
                 return
-            if i == n:
-                return
-            for j in range(i,n):
-                if j > i and candidates[i] == candidates[i-1]:
-                    continue
-                curr_sum += candidates[j]
-                curr_lst.append(candidates[j])
-                backTrack(j + 1, curr_sum, curr_lst)
-                curr_sum -= candidates[j]
-                curr_lst.pop()
 
-        backTrack(0, 0, [])
-        return list(ans)
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+
+                if curr_sum + candidates[i] > target:
+                    break
+
+                path.append(candidates[i])
+                backtrack(i + 1, curr_sum + candidates[i], path)
+                path.pop()
+
+        backtrack(0, 0, [])
+        return res
