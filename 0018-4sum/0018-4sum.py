bbb09bb2e0910ac18b8eceb5b7c1eb8fb2f55c10
Amr Harb <65994648+amrharb@users.noreply.github.com>
2025-12-15 14:45:25 +0200

    Time: 343 ms (77.03%), Space: 22.2 MB (5.77%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,8 +1,12 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
-        dict = defaultdict(list)
         n = len(nums)
+        for i in range(n - 1, 3, -1):
+            if nums[i] == nums[i - 4]:
+                nums.pop(i)
+        n = len(nums)
+        dict = defaultdict(list)
         for i in range(n):
             for j in range(i + 1, n):
                 dict[nums[i] + nums[j]].append([i, j])
