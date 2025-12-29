6c4f73b2efb8628d1d1ba9f9b88771e1b446b6eb
Amr Harb <65994648+amrharb@users.noreply.github.com>
2025-12-30 01:27:01 +0200

    Time: 98 ms (87.37%), Space: 19.9 MB (37.88%) - LeetHub

---
+++ 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.py
@@ -0,0 +1,26 @@
+class Solution:
+    def pyramidTransition(self, bottom: str, allowed: List[str]) -> bool:
+        validTops = defaultdict(set)
+        for pattern in allowed:
+            validTops[(pattern[0],pattern[1])].add(pattern[2])
+        @cache
+        def check(pattern):
+            if len(pattern)==1:
+                return True
+            choices = list()
+            choices.append("")
+            for i in range(len(pattern)-1):
+                if (pattern[i], pattern[i+1]) not in validTops:
+                    return False
+                temp = list(choices)
+                choices = []
+                for top in validTops[(pattern[i], pattern[i+1])]:
+                    for choice in temp:
+                        nw = choice + top
+                        choices.append(nw)
+                    
+            for choice in choices:
+                if check(choice):
+                    return True
+            return False
+        return check(bottom)
