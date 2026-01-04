c87698eaca87af3418748b476d43e709933d12f7
Amr Harb <65994648+amrharb@users.noreply.github.com>
2026-01-04 22:32:23 +0200

    Time: 237 ms (40.83%), Space: 23.2 MB (5.19%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.py
+++ 1390-four-divisors/1390-four-divisors.py
@@ -1,19 +1,24 @@
 class Solution:
+    def __init__(self):
+        n = 10**5 + 1
+        self.prime = [True] * n
+        self.spf = [i for i in range(n)]
+        self.prime[0] = self.prime[1] = False
+        for i in range(2, n):
+            if not self.prime[i]:
+                continue
+            for j in range(i * i, n, i):
+                if self.spf[j] == j:
+                    self.spf[j] = i
+                self.prime[j] = False
+
     def sumFourDivisors(self, nums: List[int]) -> int:
         summ = 0
         for num in nums:
-            cnt = 0
-            curr = 0
-            sq = int(sqrt(num))
-            for i in range(1, sq + 1):
-                if num % i == 0:
-                    cnt += 1
-                    curr += i
-                    if i * i != num:
-                        cnt += 1
-                        curr += num // i
-                    if cnt > 4:
-                        break
-            if cnt == 4:
-                summ += curr
+            spf = self.spf[num]
+            other = num // spf
+            if spf != num and (
+                self.prime[other] and spf != other or other == spf * spf
+            ):
+                summ += 1 + spf + other + num
         return summ
