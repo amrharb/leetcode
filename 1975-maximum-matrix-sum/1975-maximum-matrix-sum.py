d06339fa3566d169fc2ca17d77f48521a3083a40
Amr Harb <65994648+amrharb@users.noreply.github.com>
2026-01-05 06:58:55 +0200

    Time: 56 ms (87.97%), Space: 26.6 MB (15.19%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.py
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxMatrixSum(self, matrix: List[List[int]]) -> int:
-        mn = min(abs(num) for row in matrix for num in row)
-        summ = sum(abs(num) for row in matrix for num in row)
-        isNegOdd = sum(1 for row in matrix for num in row if num < 0) & 1
-        return summ - isNegOdd * 2 * mn
+        flat = [num for row in matrix for num in row]
+        return sum(map(abs, flat)) - 2 * (sum(num < 0 for num in flat) & 1) * min(
+            map(abs, flat)
+        )
