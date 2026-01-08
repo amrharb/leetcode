723d5beafa31fa0cc0df1bfaf69ed3d165c36907
Amr Harb <65994648+amrharb@users.noreply.github.com>
2026-01-08 16:06:09 +0200

    Time: 425 ms (11.56%), Space: 110.1 MB (5.53%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.py
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.py
@@ -2,20 +2,16 @@ class Solution:
     def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
         n = len(nums1)
         m = len(nums2)
-        dp = defaultdict(int)
 
-        def solve(i, j, take):
+        @cache
+        def solve(i, j):
             if i == n or j == m:
-                if not take:
-                    return -(10**18)
-                return 0
-            if (i, j, take) in dp:
-                return dp[(i, j, take)]
-            dp[(i, j, take)] = max(
-                solve(i + 1, j, take),
-                solve(i, j + 1, take),
-                nums1[i] * nums2[j] + solve(i + 1, j + 1, True),
+                return -inf
+            return max(
+                nums1[i] * nums2[j],
+                solve(i + 1, j),
+                solve(i, j + 1),
+                nums1[i] * nums2[j] + solve(i + 1, j + 1),
             )
-            return dp[(i, j, take)]
 
-        return solve(0, 0, False)
+        return solve(0, 0)
