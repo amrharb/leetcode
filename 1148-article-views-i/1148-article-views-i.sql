2b804fe96cfe51b2fb3407cdbd07148eacf7ac34
Amr Harb <65994648+amrharb@users.noreply.github.com>
2026-02-20 08:15:03 +0200

    Time: 575 ms (7.69%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,4 +1,9 @@
+with selfViews as(
+    select author_id
+    from views
+    where author_id = viewer_id
+)
+
 select distinct author_id as id
-from views
-where author_id = viewer_id
+from selfViews
 order by id;
