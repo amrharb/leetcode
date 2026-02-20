50d9096f8e0928efc9afc6af944cf924e382fe6d
Amr Harb <65994648+amrharb@users.noreply.github.com>
2026-02-20 06:52:51 +0200

    Time: 283 ms (60.04%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,8 +1,3 @@
-select
-    name,
-    population,
-    area
-from
-    World
-where
-    area >=3000000 or population >= 25000000
+select name, population, area
+from world
+where area >= 3e6 or population >= 25e6;
