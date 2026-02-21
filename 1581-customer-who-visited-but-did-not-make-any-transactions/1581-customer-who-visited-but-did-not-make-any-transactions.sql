f24cc860d6ee55918aced700d18489edc675821d
Amr Harb <65994648+amrharb@users.noreply.github.com>
2026-02-21 21:28:15 +0200

    Time: 1213 ms (81.26%), Space: 0B (100%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,13 +1,4 @@
-select
-    customer_id,
-    count(*) as count_no_trans
-from
-    visits as v
-left join
-    transactions as t
-on
-    v.visit_id = t.visit_id
-where
-    t.visit_id is null
-Group by
-    customer_id
+select customer_id, count(*) as count_no_trans
+from visits left join transactions on visits.visit_id = transactions.visit_id
+where transaction_id  is null
+group by customer_id;
