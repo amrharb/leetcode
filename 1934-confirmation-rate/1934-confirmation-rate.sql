787748f30a8a7d4ba3ba34752141d4ac73e3df77
Amr Harb <65994648+amrharb@users.noreply.github.com>
2026-02-22 22:08:21 +0200

    Time: 560 ms (97.94%), Space: 0B (100%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,3 @@
+select s.user_id as user_id, round(sum(case when action = 'confirmed' then 1 else 0 end) * 1.0 / count(*), 2) as confirmation_rate 
+from signups s left join confirmations c on s.user_id = c.user_id
+group by s.user_id;
