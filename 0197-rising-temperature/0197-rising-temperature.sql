0a33023e9d961829b84c8cfa76d79ceb08771a32
Amr Harb <65994648+amrharb@users.noreply.github.com>
2026-02-22 14:18:42 +0200

    Time: 354 ms (92.81%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,10 +1,4 @@
-with cte as
-(
-    select w1.id as id, w1.temperature temp, w2.temperature as prev_temp
-    from weather w1 left join weather w2
-    on w1.recordDate = date_add(w2.recordDate, interval 1 day)
-)
-
-select id
-from cte
-where temp > prev_temp;
+select w1.id as id
+from weather w1 left join weather w2
+on w1.recordDate = date_add(w2.recordDate, interval 1 day)
+where w1.temperature > w2.temperature;
