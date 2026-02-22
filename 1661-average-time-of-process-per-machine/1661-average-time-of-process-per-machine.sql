b79bf1e9450548250ca95ac34b03bea310f7a3bb
Amr Harb <65994648+amrharb@users.noreply.github.com>
2026-02-22 15:10:58 +0200

    Time: 229 ms (81.45%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,4 +1,9 @@
-select machine_id as machine_id, 
-        round(sum((case when activity_type = 'end' then timestamp else -timestamp end)) / count(*) * 2,3) as processing_time
-from activity
+with cte as
+(
+    select machine_id, max(timestamp) - min(timestamp) as diff
+    from activity
+    group by machine_id, process_id
+)
+select machine_id, round(avg(diff), 3) as processing_time
+from cte
 group by machine_id;
