40bee0a941345cc915b636d06418269c5fab328b
Amr Harb <65994648+amrharb@users.noreply.github.com>
2026-02-22 15:14:14 +0200

    Time: 968 ms (45.41%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,4 +1,10 @@
-select e.name,b.bonus
-from Employee e left join Bonus b
-on e.empId=b.empId
-where b.bonus<1000 or b.bonus is null;
+with cte as
+(
+    select name, bonus
+    from employee e left join bonus b
+    on e.empId = b.empId
+)
+
+select *
+from cte
+where bonus is null or bonus < 1000;
