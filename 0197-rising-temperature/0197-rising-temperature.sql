e0b9a4eab267b09fdc82d40464bbaec13559cb84
Amr Harb <65994648+amrharb@users.noreply.github.com>
2026-02-22 14:33:49 +0200

    Time: 913 ms (5.67%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,12 +1,7 @@
-with cte as
-(
-    select id,
-           temperature,
-           recordDate,
-           lag(temperature) over (order by recordDate) as prev_temp,
-           lag(recordDate) over (order by recordDate) as prev_date
-    from weather
-)
-select Id
-from cte
-where temperature > prev_temp and datediff(recordDate, prev_date) = 1;
+select id
+from weather w1
+where temperature > (
+    select temperature
+    from weather w2
+    where datediff(w1.recordDate, w2.recordDate) = 1
+);
