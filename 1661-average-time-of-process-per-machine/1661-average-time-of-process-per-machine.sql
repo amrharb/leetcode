636c1ec922d313aadd5ac1bf418db895c2b0b16d
Amr Harb <65994648+amrharb@users.noreply.github.com>
2026-02-22 14:53:56 +0200

    Time: 229 ms (81.45%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -3,6 +3,6 @@ with cte1 as
     select a1.machine_id as machine_id, a2.timestamp - a1.timestamp as diff
     from activity a1 join activity a2 on a1.activity_type = "start" and a2.activity_type = "end" and a1.process_id = a2.process_id and a1.machine_id = a2.machine_id
 )
-select machine_id , round(sum(diff) / count(*), 3) as processing_time
+select machine_id , round(avg(diff), 3) as processing_time
 from cte1
 group by machine_id;
