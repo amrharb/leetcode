ab5b705ba8cf2e78ec78c95ecb3cdcaf88838a52
Amr Harb <65994648+amrharb@users.noreply.github.com>
2026-02-22 14:17:38 +0200

    Time: 379 ms (79.74%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,10 +1,10 @@
-select distinct
-    w1.id
-from
-    weather w1
-left join
-    weather w2
-on
-    datediff(w2.recordDate,w1.recordDate)=-1
-where
-    w1.temperature > w2.temperature
+with cte as
+(
+    select w1.id as id, w1.temperature temp, w2.temperature as prev_temp
+    from weather w1 left join weather w2
+    on w1.recordDate = date_add(w2.recordDate, interval 1 day)
+)
+
+select id
+from cte
+where temp > prev_temp;
