0f69d8e714331b581bab7503ab0ebcb948fcb03a
Amr Harb <65994648+amrharb@users.noreply.github.com>
2026-02-25 14:22:05 +0200

    Time: 6 ms (21.84%), Space: 14.3 MB (58.88%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
-        sort(arr.begin(),arr.end(),[&](int a,int b){
-            bitset<32>s(a),x(b);
-            if(s.count()!=x.count())return s.count()<x.count();
-            return a<b;
+        sort(arr.begin(), arr.end(), [&](int a, int b) {
+            int bits_1 = bitset<14>(a).count();
+            int bits_2 = bitset<14>(b).count();
+            if (bits_1 != bits_2)
+                return bits_1 < bits_2;
+            return a < b;
         });
         return arr;
     }
