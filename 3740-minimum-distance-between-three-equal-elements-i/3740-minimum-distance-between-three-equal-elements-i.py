2ab2618aeb57a49d2d714c6bdc196dc5dae519b8
Amr Harb <65994648+amrharb@users.noreply.github.com>
2026-04-11 01:55:18 +0200

    Time: 3 ms (67.31%), Space: 19.3 MB (46.28%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.py
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.py
@@ -1,14 +1,22 @@
 class Solution:
     def minimumDistance(self, nums: List[int]) -> int:
-        ans = 10 ** 18
+        ans = 10**18
         n = len(nums)
-        adj = [[] for _ in range(n+1)]
+        adj = defaultdict(list)
         for i in range(n):
             adj[nums[i]].append(i)
-        for num in range(n+1):
+        for num in adj:
             if len(adj[num]) > 2:
-                for j in range(2,len(adj[num])):
-                    ans = min(ans, adj[num][j-1] - adj[num][j-2] + adj[num][j] - adj[num][j-1] + adj[num][j] - adj[num][j-2])
-        if ans == 10 ** 18:
+                for j in range(2, len(adj[num])):
+                    ans = min(
+                        ans,
+                        adj[num][j - 1]
+                        - adj[num][j - 2]
+                        + adj[num][j]
+                        - adj[num][j - 1]
+                        + adj[num][j]
+                        - adj[num][j - 2],
+                    )
+        if ans == 10**18:
             return -1
         return ans
